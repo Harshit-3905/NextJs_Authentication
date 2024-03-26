b93939fc29fd085c868c8cd56018718eca130b8d
Harshit-3905 <harshitjoshileet@gmail.com>
2024-03-27 03:47:34 +0530

    Fix typo in emailType value in user signup route

---
+++ src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,33 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+
+connectDB();
+
+export async function POST(req: NextRequest) {
+  try {
+    const reqBody = await req.json();
+    const token = reqBody.token;
+    console.log(token);
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpire: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "Token is invalid or has expired" },
+        { status: 400 }
+      );
+    }
+
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpire = undefined;
+    await user.save();
+
+    return NextResponse.json({ message: "Email Verified" }, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
